eaeb4c3b0192d83492a86c50138aa0467d653490
Kenneth Endfinger <kaendfinger@gmail.com>
2014-12-14 16:24:25 -0500

    Remove the linux examples for now :)

--- example/rollup.dart
+++ example/rollup.dart
@@ -1,6 +1,8 @@
 import "package:dslink/api.dart";
 
 void main() {
-  var values = [1, 2, 3, 4].map((it) => Value.of(it)).toList();
+  var values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].map(wrapValue).toList();
   print(RollupType.SUM.create().combine(values));
 }
+
+Value wrapValue(input) => Value.of(input);
