8a2d49aa8b2bb73262829db8971817bcc0bebc1e
Kenneth Endfinger <kaendfinger@gmail.com>
2014-12-17 13:26:03 -0500

    Add analyzer to grinder

--- tool/grind.dart
+++ tool/grind.dart
@@ -2,11 +2,19 @@ import "package:grinder/grinder.dart";
 
 void main(List<String> args) {
   task("test", runTests);
+  task("analyze", analyze);
   startGrinder(args);
 }
 
 void runTests(GrinderContext context) {
   Tests.runCliTests(context, [
     "api_tests.dart"
+  ]);
+}
+
+void analyze(GrinderContext context) {
+  Analyzer.analyzePaths(context, [
+    "lib/link.dart",
+    "lib/link_browser.dart"
   ]);
 }
