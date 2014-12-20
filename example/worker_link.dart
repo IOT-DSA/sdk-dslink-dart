2c12b2cd1923c50f696ee8ef1c971b814580caa7
Kenneth Endfinger <kaendfinger@gmail.com>
2014-12-20 12:53:02 -0500

    Add Worker Link Example

---
+++ example/worker_link.dart
@@ -0,0 +1,47 @@
+import "package:dslink/link.dart";
+import "package:dslink/isolation.dart";
+import "dart:async";
+
+DSNode counterNode;
+void main(args) {
+  var link = new DSLink("DartIsolateLink", host: "rnd.iot-dsa.org", debug: args.contains("-d") || args.contains("--debug"));
+  
+  link.loadNodes([
+    {
+      "name": "Counter",
+      "value": 0,
+      "initialize": (node) => counterNode = node
+    }
+  ]);
+  
+  link.connect().then((_) {
+    print("Connected.");
+    _startWorker();
+  });
+}
+
+void _startWorker() {
+  var socket = spawnWorker(worker);
+  
+  socket.waitFor().then((_) {
+    socket.listen((value) {
+      counterNode.value = value;
+    });
+  });
+}
+
+void worker(port) {
+  var socket = new WorkerSocket.worker(port);
+  Timer timer;
+  
+  socket.done.then((_) {
+    timer.cancel();
+  });
+  
+  int i = 0;
+  
+  timer = new Timer.periodic(new Duration(seconds: 1), (_) {
+    i++;
+    socket.add(i);
+  });
+}
--- lib/isolation.dart
+++ lib/isolation.dart
@@ -5,3 +5,12 @@ import "dart:isolate";
 import "dart:async";
 
 part "src/isolation/worker.dart";
+
+typedef void WorkerFunction(port);
+
+WorkerSocket spawnWorker(WorkerFunction function) {
+  var receiver = new ReceivePort();
+  Isolate.spawn(function, receiver.sendPort);
+  var socket = new WorkerSocket.master(receiver);
+  return socket;
+}
