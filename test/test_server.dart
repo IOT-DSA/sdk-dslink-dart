f2a8545025bcceb0072573de299ee2b5e869c934
Rick Zhou <rinick@gmail.com>
2015-04-13 17:04:19 -0700

    improve test server example so it starts like a regular dslink responder

--- test/test_server.dart
+++ test/test_server.dart
@@ -1,14 +1,19 @@
 import 'dart:io';
 import 'package:dslink/server.dart';
 import 'package:dslink/broker.dart';
+import 'package:dslink/client.dart';
 
-void main() {
+void main(List<String> args) {
   // load certificate
   String certPath = Platform.script.resolve('certs').toFilePath();
   SecureSocket.initialize(database: certPath, password: 'mypassword');
 
   // start the server
   var broker = new BrokerNodeProvider();
-  var server = new DsHttpServer.start(InternetAddress.ANY_IP_V4,
+  var server = new DsHttpServer.start(InternetAddress.ANY_IP_V4, httpPort:8088,
       certificateName: "self signed for dart", nodeProvider: broker, linkManager: broker);
+  
+  if (args.contains('--broker') || args.contains('-b')) {
+    new LinkProvider(args, 'broker-', nodeProvider:broker).connect();
+  }
 }
