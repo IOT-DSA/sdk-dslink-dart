a5fbc303e233a172ec7f5d4f212d1a43ff20c0ed
Kenneth Endfinger <kaendfinger@gmail.com>
2015-04-13 22:36:55 -0400

    Bug Fix Attempt

---
+++ test/large.dart
@@ -0,0 +1,27 @@
+import 'package:dslink/client.dart';
+import 'package:dslink/utils.dart';
+import 'package:dslink/responder.dart';
+import 'dart:math' as Math;
+import 'dart:async';
+
+LinkProvider link;
+
+main(List<String> args){
+  link = new LinkProvider(args, 'large-link-');
+
+  if (link.link == null) {
+    // initialization failed
+    return;
+  }
+
+
+  for (var x in new List<int>.generate(500, (i) => i)) {
+    link.provider.addNode("/Node_${x}", {
+      "?value": "node ${x}",
+      r"$type": "string"
+    });
+  }
+
+
+  link.connect();
+}
