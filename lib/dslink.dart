1d010ae0ab496430afc4a8686aec1abd3a14857b
Kenneth Endfinger <kaendfinger@gmail.com>
2015-05-11 21:57:37 -0400

    Add package:dslink/dslink.dart

---
+++ lib/dslink.dart
@@ -0,0 +1,8 @@
+/// DSLink SDK for the Dart VM
+/// Requires dart:io
+library dslink;
+
+export "package:dslink/common.dart";
+export "package:dslink/requester.dart";
+export "package:dslink/responder.dart";
+export "package:dslink/client.dart";
