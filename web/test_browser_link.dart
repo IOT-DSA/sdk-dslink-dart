7d74a788c262ba733c9735e18d412c0d3d930b7b
Rick Zhou <rinick@gmail.com>
2015-05-18 21:46:43 -0700

    fix errors

--- web/test_browser_link.dart
+++ web/test_browser_link.dart
@@ -4,7 +4,7 @@
 import 'dart:html';
 import 'package:dslink/src/crypto/pk.dart';
 import 'package:dslink/browser_client.dart';
-import '../test/sample_responder.dart';
+import '../test/experiment/sample_responder.dart';
 
 void main() {
   querySelector('#output').text = 'Your Dart app is running.';
