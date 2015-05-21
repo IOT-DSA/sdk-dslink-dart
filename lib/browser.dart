51de6e0c85fa226b6a4d9c12af9d6575756ca098
Kenneth Endfinger <kaendfinger@gmail.com>
2015-05-21 11:31:17 -0400

    Add Browser EntryPoint Library

---
+++ lib/browser.dart
@@ -0,0 +1,9 @@
+/// Main DSLink API for Browsers
+library dslink.browser;
+
+export "package:dslink/common.dart";
+export "package:dslink/requester.dart";
+export "package:dslink/responder.dart";
+export "package:dslink/browser_client.dart";
+export "package:dslink/utils.dart" show Scheduler, Interval, DSLinkJSON;
+export "package:dslink/src/crypto/pk.dart" show PrivateKey;
