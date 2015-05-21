adb3ff900424b64303c84b88c0018eacafd920a0
Kenneth Endfinger <kaendfinger@gmail.com>
2015-05-21 11:59:39 -0400

    Export updateLogLevel

--- lib/browser.dart
+++ lib/browser.dart
@@ -14,7 +14,7 @@ export "package:dslink/common.dart";
 export "package:dslink/requester.dart";
 export "package:dslink/responder.dart";
 export "package:dslink/browser_client.dart";
-export "package:dslink/utils.dart" show Scheduler, Interval, DSLinkJSON;
+export "package:dslink/utils.dart" show Scheduler, Interval, DSLinkJSON, updateLogLevel;
 export "package:dslink/src/crypto/pk.dart" show PrivateKey;
 
 /// DSLink Provider for the Browser
