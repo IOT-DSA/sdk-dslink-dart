4fe008b145063350664fcf8402abbd5df84f0f06
Kenneth Endfinger <kaendfinger@gmail.com>
2015-05-23 12:17:25 -0400

    Add IO Library

---
+++ lib/io.dart
@@ -0,0 +1,11 @@
+/// DSLink SDK IO Utilities
+library dslink.io;
+
+import "dart:async";
+import "dart:convert";
+import "dart:io";
+
+Stream<String> readStdinText() =>
+  stdin.transform(UTF8.decoder);
+Stream<String> readStdinLines() =>
+  readStdinText().transform(new LineSplitter());
