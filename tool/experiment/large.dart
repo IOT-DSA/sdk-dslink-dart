05a1164ed24d7bef0cb3a6006d85cf99567754cc
Kenneth Endfinger <kaendfinger@gmail.com>
2015-05-24 00:41:56 -0400

    Large Nodes Link Test: Add Reduce action.

--- tool/experiment/large.dart
+++ tool/experiment/large.dart
@@ -17,36 +17,58 @@ main(List<String> args) {
           "default": 50
         }
       ]
+    },
+    "Reduce": {
+      r"$invokable": "write",
+      r"$is": "reduce",
+      r"$params": [
+        {
+          "name": "target",
+          "type": "number",
+          "default": 1
+        }
+      ]
     }
   }, profiles: {
     "generate": (String path) => new SimpleActionNode(path, (Map<String, dynamic> params) {
       var count = params["count"] != null ? params["count"] : 50;
-      for (var i = 1; i <= count; i++) {
-        link.addNode("/Node_${i}", {
-          r"$name": "Node ${i}",
-          "String_Value": {
-            r"$name": "String Value",
-            r"$type": "string",
-            r"$writable": "write",
-            "?value": "Hello World"
-          },
-          "Number_Value": {
-            r"$name": "Number Value",
-            r"$type": "number",
-            r"$writable": "write",
-            "?value": 5.0
-          },
-          "Integer_Value": {
-            r"$name": "Integer Value",
-            r"$type": "number",
-            r"$writable": "write",
-            "?value": 5
-          }
-        });
-        current++;
+      generate(count);
+    }),
+    "reduce": (String path) => new SimpleActionNode(path, (Map<String, dynamic> params) {
+      var target = params["target"] != null ? params["target"] : 1;
+      for (var name in link["/"].children.keys.where((it) => it.startsWith("Node_"))) {
+        link.removeNode("/${name}");
       }
+      generate(target);
     })
   });
 
   link.connect();
+}
+
+void generate(int count) {
+  for (var i = 1; i <= count; i++) {
+    link.addNode("/Node_${i}", {
+      r"$name": "Node ${i}",
+      "String_Value": {
+        r"$name": "String Value",
+        r"$type": "string",
+        r"$writable": "write",
+        "?value": "Hello World"
+      },
+      "Number_Value": {
+        r"$name": "Number Value",
+        r"$type": "number",
+        r"$writable": "write",
+        "?value": 5.0
+      },
+      "Integer_Value": {
+        r"$name": "Integer Value",
+        r"$type": "number",
+        r"$writable": "write",
+        "?value": 5
+      }
+    });
+    current++;
+  }
 }
