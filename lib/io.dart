0ac168e0a065d21d5b44fd0e617dfc3c6707fb57
Kenneth Endfinger <kaendfinger@gmail.com>
2015-06-30 01:29:01 -0400

    Add HttpHelper

--- lib/io.dart
+++ lib/io.dart
@@ -8,3 +8,32 @@ import "dart:io";
 Stream<String> readStdinText() => stdin.transform(UTF8.decoder);
 Stream<String> readStdinLines() =>
     readStdinText().transform(new LineSplitter());
+
+class HttpHelper {
+  static HttpClient client = new HttpClient();
+
+  static Future<HttpClientRequest> createRequest(String method, String url, {Map<String, String> headers}) async {
+    var request = await client.openUrl(method, Uri.parse(url));
+    if (headers != null) {
+      headers.forEach(request.headers.set);
+    }
+    return request;
+  }
+
+  static Future<List<int>> readBytesFromResponse(HttpClientResponse response) async {
+    return await response.fold([], (a, b) {
+      a.addAll(b);
+      return a;
+    });
+  }
+
+  static Future<String> fetchUrl(String url, {Map<String, String> headers}) async {
+    var request = await createRequest("GET", url, headers: headers);
+    var response = await request.close();
+    return UTF8.decode(await readBytesFromResponse(response));
+  }
+
+  static Future<dynamic> fetchJSON(String url, {Map<String, String> headers}) async {
+    return JSON.decode(await fetchUrl(url, headers: headers));
+  }
+}
