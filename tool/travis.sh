f384a195782a60f33080ff87377cdca70c52d936
Kenneth Endfinger <kaendfinger@gmail.com>
2015-07-10 20:35:57 -0400

    Fix Travis CI Build

--- tool/travis.sh
+++ tool/travis.sh
@@ -3,7 +3,8 @@ set -e
 
 ./tool/analyze.sh
 ./tool/test.sh
-if [ "${TRAVIS_DART_VERSION}" == "stable" ]
+if [ "${TRAVIS_DART_VERSION}" == "stable" ] && [ "${TRAVIS_PULL_REQUEST}" == "false" ]
 then
+  openssl aes-256-cbc -K $encrypted_afe27f9b0c58_key -iv $encrypted_afe27f9b0c58_iv -in tool/id_rsa.enc -out ~\/.ssh/id_rsa -d
   ./tool/docs.sh --upload
 fi
