475fbd534bb717e087fe74a1d6eacc86790e8044
Kenneth Endfinger <kaendfinger@gmail.com>
2015-07-10 20:43:30 -0400

    Add Identity for CI

--- tool/travis.sh
+++ tool/travis.sh
@@ -10,6 +10,8 @@ then
     mkdir ${HOME}/.ssh
   fi
 
+  git config --global user.name "Travis CI"
+  git config --global user.email "travis@iot-dsa.org"
   openssl aes-256-cbc -K $encrypted_afe27f9b0c58_key -iv $encrypted_afe27f9b0c58_iv -in tool/id_rsa.enc -out ${HOME}/.ssh/id_rsa -d
   chmod 600 ${HOME}/.ssh/id_rsa
   echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> ${HOME}/.ssh/config
