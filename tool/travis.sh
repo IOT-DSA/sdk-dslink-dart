eb478a7e04d548b9965f2947582833ad403512d2
Kenneth Endfinger <kaendfinger@gmail.com>
2015-07-10 20:41:30 -0400

    Bug Fix in Build Script

--- tool/travis.sh
+++ tool/travis.sh
@@ -5,13 +5,13 @@ set -e
 ./tool/test.sh
 if [ "${TRAVIS_DART_VERSION}" == "stable" ] && [ "${TRAVIS_PULL_REQUEST}" == "false" ]
 then
-  if [ ! -d ~/.ssh ]
+  if [ ! -d ${HOME}/.ssh ]
   then
-    mkdir ~/.ssh
+    mkdir ${HOME}/.ssh
   fi
-  
-  openssl aes-256-cbc -K $encrypted_afe27f9b0c58_key -iv $encrypted_afe27f9b0c58_iv -in tool/id_rsa.enc -out ~\/.ssh/id_rsa -d
-  chmod 600 ~/.ssh/id_rsa
-  echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+
+  openssl aes-256-cbc -K $encrypted_afe27f9b0c58_key -iv $encrypted_afe27f9b0c58_iv -in tool/id_rsa.enc -out ${HOME}/.ssh/id_rsa -d
+  chmod 600 ${HOME}/.ssh/id_rsa
+  echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> ${HOME}/.ssh/config
   ./tool/docs.sh --upload
 fi
