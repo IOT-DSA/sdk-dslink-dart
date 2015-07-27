669ab3ad9f3ccd9935b6fd7d657c9558353949bc
Kenneth Endfinger <kaendfinger@gmail.com>
2015-07-26 23:12:31 -0400

    Add Test Coverage

---
+++ test/utils/all.dart
@@ -0,0 +1,11 @@
+import "action_test.dart" as ActionTests;
+import "base64_test.dart" as Base64Tests;
+import "logging_test.dart" as LoggingTests;
+import "scheduler_test.dart" as SchedulerTests;
+
+main() {
+  ActionTests.main();
+  Base64Tests.main();
+  LoggingTests.main();
+  SchedulerTests.main();
+}
--- tool/travis.sh
+++ tool/travis.sh
@@ -3,7 +3,7 @@ set -e
 
 ./tool/analyze.sh
 ./tool/test.sh
-if [ "${TRAVIS_DART_VERSION}" == "stable" ] && [ "${TRAVIS_PULL_REQUEST}" == "false" ]
+if [ "${TRAVIS_DART_VERSION}" == "stable" ] && [ "${TRAVIS_PULL_REQUEST}" == "false" ] && [ "${TRAVIS_BRANCH}" == "develop" ]
 then
   if [ ! -d ${HOME}/.ssh ]
   then
@@ -16,4 +16,14 @@ then
   chmod 600 ${HOME}/.ssh/id_rsa
   echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> ${HOME}/.ssh/config
   ./tool/docs.sh --upload
+fi
+
+if [ "${COVERALLS_TOKEN}" ]
+then
+  pub global activate dart_coveralls
+  pub global run dart_coveralls report \
+    --token ${COVERALLS_TOKEN} \
+    --retry 2 \
+    --exclude-test-files \
+    test/all.dart
 fi
