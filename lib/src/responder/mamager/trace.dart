06ec98b5f096481659991586d17fbe4d3c2fe768
Rick Zhou <rinick@gmail.com>
2015-07-28 02:50:25 -0700

    implement trace action

---
+++ lib/src/responder/mamager/trace.dart
@@ -0,0 +1,30 @@
+part of dslink.responder;
+
+
+typedef ResponseTraceCallback(ResponseTrace update);
+
+/// 
+class ResponseTrace {
+  
+  /// data path for trace
+  String path;
+  /// 'list' 'subscribe' 'invoke'
+  String type;
+  
+  /// value is + - or blank string
+  String change;
+  
+  /// action name, only needed by invoke
+  String action;
+  /// rid, only needed by invoke
+  int rid;
+  
+//  {'name': 'path', 'type': 'string'},
+//  {'name': 'type', 'type': 'string'},
+//  {'name': 'rid', 'type': 'number'},
+//  {'name': 'action', 'type': 'string'},
+//  {'name': 'add', 'type': 'bool'},
+  List get rowData => [path, type, rid, action, change];
+  
+  ResponseTrace(this.path, this.type, this.rid, [this.change = '', this.action]);
+}
