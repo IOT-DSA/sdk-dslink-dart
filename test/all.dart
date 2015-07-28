539f1af84026ccdd97b0c5fc80a9865c9f2b600d
Kenneth Endfinger <kaendfinger@gmail.com>
2015-07-28 00:16:36 -0400

    Add Tests for Broker Discovery

--- test/all.dart
+++ test/all.dart
@@ -2,10 +2,12 @@ import "simple_links_test.dart" as SimpleLinksTests;
 import "simple_nodes_test.dart" as SimpleNodesTests;
 import "worker_test.dart" as WorkerTests;
 import "utils/all.dart" as UtilsTests;
+import "broker_discovery_test.dart" as BrokerDiscoveryTests;
 
 main() {
   UtilsTests.main();
   SimpleLinksTests.main();
   SimpleNodesTests.main();
   WorkerTests.main();
+  BrokerDiscoveryTests.main();
 }
---
+++ test/broker_discovery_test.dart
@@ -0,0 +1,27 @@
+@TestOn("vm")
+@Timeout(const Duration(seconds: 10))
+library dslink.test.vm.discovery;
+
+import "package:dslink/broker.dart" show BrokerDiscoveryClient, BrokerDiscoverRequest;
+
+import "package:test/test.dart";
+
+main() => group("Broker Discovery", brokerDiscoveryTests);
+
+brokerDiscoveryTests() {
+  test("works with a single broker", () async {
+    var host = new BrokerDiscoveryClient();
+    await host.init(true);
+    host.requests.listen((request) {
+      request.reply("http://127.0.0.1:8080");
+    });
+
+    var client = new BrokerDiscoveryClient();
+    await client.init();
+    var urls = await client.discover().toList();
+    expect(urls.length, equals(1));
+    expect(urls.first, equals("http://127.0.0.1:8080"));
+    await host.close();
+    await client.close();
+  });
+}
