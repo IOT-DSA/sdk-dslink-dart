e609a6d6934d47d49582c1fc215b64f7e208c40d
Kenneth Endfinger <kaendfinger@gmail.com>
2015-08-21 12:28:02 -0400

    Add Test for JSON

---
+++ test/utils/json_test.dart
@@ -0,0 +1,238 @@
+library dslink.test.common.json;
+
+import "dart:convert";
+
+import "package:test/test.dart";
+import "package:dslink/utils.dart" show DsJson, BinaryInCache, BinaryOutCache, BinaryData;
+
+void main() {
+  group("JSON", jsonTests);
+}
+
+void jsonTests() {
+  test("successfully decodes valid inputs", () {
+    for (var i = 0; i < JSON_STRINGS.length; i++) {
+      var input = JSON_STRINGS[i];
+      var output = JSON_OBJECTS[i];
+
+      expect(DsJson.decode(input), equals(output));
+    }
+  });
+
+  test("successfully encodes valid objects", () {
+    for (var i = 0; i < JSON_STRINGS.length; i++) {
+      var input = JSON_OBJECTS[i];
+      var output = JSON_STRINGS[i];
+
+      expect(DsJson.encode(input, pretty: true), equals(output.trim()));
+    }
+  });
+}
+
+final List<String> JSON_STRINGS = [
+  """
+{
+  "responses": [
+    {
+      "rid": 0,
+      "updates": [
+        {
+          "ts": "2015-08-17T18:31:49.856+00:00",
+          "value": 1,
+          "sid": 5,
+          "count": 36332523,
+          "sum": 18165428,
+          "max": 1,
+          "min": 0
+        },
+        {
+          "ts": "2015-08-17T18:31:49.856+00:00",
+          "value": 1,
+          "sid": 6,
+          "count": 36332523,
+          "sum": 18165428,
+          "max": 1,
+          "min": 0
+        },
+        {
+          "ts": "2015-08-17T18:31:49.856+00:00",
+          "value": 1,
+          "sid": 7,
+          "count": 36332523,
+          "sum": 18165428,
+          "max": 1,
+          "min": 0
+        },
+        {
+          "ts": "2015-08-17T18:31:49.856+00:00",
+          "value": 1,
+          "sid": 8,
+          "count": 36332523,
+          "sum": 18165428,
+          "max": 1,
+          "min": 0
+        },
+        {
+          "ts": "2015-08-17T18:31:49.856+00:00",
+          "value": 1,
+          "sid": 9,
+          "count": 36332523,
+          "sum": 18165428,
+          "max": 1,
+          "min": 0
+        },
+        {
+          "ts": "2015-08-17T18:31:49.856+00:00",
+          "value": 1,
+          "sid": 10,
+          "count": 36332523,
+          "sum": 18165428,
+          "max": 1,
+          "min": 0
+        },
+        {
+          "ts": "2015-08-17T18:31:49.856+00:00",
+          "value": 1,
+          "sid": 11,
+          "count": 36332523,
+          "sum": 18165428,
+          "max": 1,
+          "min": 0
+        },
+        {
+          "ts": "2015-08-17T18:31:49.856+00:00",
+          "value": 1,
+          "sid": 12,
+          "count": 36332523,
+          "sum": 18165428,
+          "max": 1,
+          "min": 0
+        },
+        {
+          "ts": "2015-08-17T18:31:49.856+00:00",
+          "value": 1,
+          "sid": 13,
+          "count": 36332523,
+          "sum": 18165428,
+          "max": 1,
+          "min": 0
+        },
+        {
+          "ts": "2015-08-17T18:31:49.856+00:00",
+          "value": 1,
+          "sid": 14,
+          "count": 36332523,
+          "sum": 18165428,
+          "max": 1,
+          "min": 0
+        }
+      ]
+    }
+  ],
+  "msg": 624
+}
+  """
+];
+
+final List<dynamic> JSON_OBJECTS = [
+  {
+    "responses": [
+      {
+        "rid": 0,
+        "updates": [
+          {
+            "ts": "2015-08-17T18:31:49.856+00:00",
+            "value": 1,
+            "sid": 5,
+            "count": 36332523,
+            "sum": 18165428,
+            "max": 1,
+            "min": 0
+          },
+          {
+            "ts": "2015-08-17T18:31:49.856+00:00",
+            "value": 1,
+            "sid": 6,
+            "count": 36332523,
+            "sum": 18165428,
+            "max": 1,
+            "min": 0
+          },
+          {
+            "ts": "2015-08-17T18:31:49.856+00:00",
+            "value": 1,
+            "sid": 7,
+            "count": 36332523,
+            "sum": 18165428,
+            "max": 1,
+            "min": 0
+          },
+          {
+            "ts": "2015-08-17T18:31:49.856+00:00",
+            "value": 1,
+            "sid": 8,
+            "count": 36332523,
+            "sum": 18165428,
+            "max": 1,
+            "min": 0
+          },
+          {
+            "ts": "2015-08-17T18:31:49.856+00:00",
+            "value": 1,
+            "sid": 9,
+            "count": 36332523,
+            "sum": 18165428,
+            "max": 1,
+            "min": 0
+          },
+          {
+            "ts": "2015-08-17T18:31:49.856+00:00",
+            "value": 1,
+            "sid": 10,
+            "count": 36332523,
+            "sum": 18165428,
+            "max": 1,
+            "min": 0
+          },
+          {
+            "ts": "2015-08-17T18:31:49.856+00:00",
+            "value": 1,
+            "sid": 11,
+            "count": 36332523,
+            "sum": 18165428,
+            "max": 1,
+            "min": 0
+          },
+          {
+            "ts": "2015-08-17T18:31:49.856+00:00",
+            "value": 1,
+            "sid": 12,
+            "count": 36332523,
+            "sum": 18165428,
+            "max": 1,
+            "min": 0
+          },
+          {
+            "ts": "2015-08-17T18:31:49.856+00:00",
+            "value": 1,
+            "sid": 13,
+            "count": 36332523,
+            "sum": 18165428,
+            "max": 1,
+            "min": 0
+          },
+          {
+            "ts": "2015-08-17T18:31:49.856+00:00",
+            "value": 1,
+            "sid": 14,
+            "count": 36332523,
+            "sum": 18165428,
+            "max": 1,
+            "min": 0
+          }
+        ]
+      }
+    ],
+    "msg": 624
+  }
+];
