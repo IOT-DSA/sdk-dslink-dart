15343ecb6957dd847122bf46f39d92078105cff4
Rick Zhou <rinick@gmail.com>
2015-09-08 01:58:15 -0700

    use path based file constructor instead of uri, because of this bug: https://github.com/dart-lang/sdk/issues/24304

--- lib/src/storage/simple_storage.dart
+++ lib/src/storage/simple_storage.dart
@@ -1,4 +1,4 @@
-library dslink.storage.base;
+library dslink.storage.simple;
 
 import '../../responder.dart';
 import '../../common.dart';
@@ -19,7 +19,7 @@ class SimpleStorageManager implements ISubscriptionStorageManager {
     if (rsponders.containsKey(dsId)) {
       return rsponders[dsId];
     }
-    SimpleResponderStorage responder = new SimpleResponderStorage(dsId, dir);
+    SimpleResponderStorage responder = new SimpleResponderStorage('${dir.path}/$dsId');
     rsponders[dsId] = responder;
     return responder;
   }
@@ -38,12 +38,11 @@ class SimpleStorageManager implements ISubscriptionStorageManager {
 }
 
 class SimpleResponderStorage extends ISubscriptionResponderStorage {
-  final String dsId;
   Map<String, SimpleValueStorage> values =
       new Map<String, SimpleValueStorage>();
   Directory dir;
-  SimpleResponderStorage(this.dsId, Directory parentDir) {
-    dir = new Directory.fromUri(parentDir.uri.resolve(dsId));
+  SimpleResponderStorage(String path) {
+    dir = new Directory(path);
     if (!dir.existsSync()) {
       dir.createSync(recursive: true);
     }
@@ -52,7 +51,7 @@ class SimpleResponderStorage extends ISubscriptionResponderStorage {
     if (values.containsKey(path)) {
       return values[path];
     }
-    SimpleValueStorage value = new SimpleValueStorage(path, dir);
+    SimpleValueStorage value = new SimpleValueStorage(path, dir.path);
     values[path] = value;
     return value;
   }
@@ -60,7 +59,7 @@ class SimpleResponderStorage extends ISubscriptionResponderStorage {
     for (FileSystemEntity entity in dir.listSync()){
       String name = entity.uri.pathSegments.last;
       String path = Uri.decodeComponent(name);
-      values[path] = new SimpleValueStorage(path, dir);
+      values[path] = new SimpleValueStorage(path, dir.path);
     }
     return values;
   }
@@ -82,11 +81,12 @@ class SimpleResponderStorage extends ISubscriptionResponderStorage {
 class SimpleValueStorage extends ISubscriptionValueStorage {
   File file;
   final String path;
-  SimpleValueStorage(this.path, Directory parentDir) {
-    file = new File.fromUri(parentDir.uri.resolve(Uri.encodeComponent(path)));
+  SimpleValueStorage(this.path, String parentPath) {
+    file = new File('$parentPath/${Uri.encodeComponent(path)}');
   }
   /// add data to List of values
   void addValue(ValueUpdate value) {
+    qos = 3;
     super.addValue(value);
     file.openSync(mode: FileMode.APPEND)
       ..writeStringSync(value.serialized)
@@ -94,8 +94,10 @@ class SimpleValueStorage extends ISubscriptionValueStorage {
       ..closeSync();
   }
   void setValue(ValueUpdate value) {
+    qos = 2;
     super.setValue(value);
-    file.writeAsStringSync(value.serialized);
+    // add a space when qos = 2
+    file.writeAsStringSync(' ${value.serialized}');
   }
   void removeValue(ValueUpdate value) {
     // do nothing, it's done in valueRemoved
@@ -110,13 +112,19 @@ class SimpleValueStorage extends ISubscriptionValueStorage {
   List<ValueUpdate> loadAll() {
     String str = file.readAsStringSync();
     List<String> strs = str.split('\n');
+    if (strs.length == 1 && str.startsWith(' ')){
+      // where there is space, it's qos 2
+      qos = 2;
+    } else {
+      qos = 3;
+    }
     List<ValueUpdate> rslt = new List<ValueUpdate>();
     for (String s in strs) {
       try {
-        Map m = DsJson.decode(str);
+        Map m = DsJson.decode(s);
         ValueUpdate value = new ValueUpdate(m['value'], ts: m['ts'], meta: m);
         rslt.add(value);
-      } catch (err) {}
+      } catch (err) {print(err);}
     }
     return rslt;
   }
