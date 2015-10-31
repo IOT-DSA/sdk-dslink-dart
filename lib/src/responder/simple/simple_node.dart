5aa1784d4d19a5bc2372ee39123619e12a85bdcd
Kenneth Endfinger <kaendfinger@gmail.com>
2015-10-30 21:34:12 -0400

    Revert "Revert "Implement mutable profiles in SimpleNodeProvider.""
    This reverts commit 958db982e8eec13aa612d520f888f9e116f6f98b.

--- lib/src/responder/simple/simple_node.dart
+++ lib/src/responder/simple/simple_node.dart
@@ -1,6 +1,8 @@
 part of dslink.responder;
 
 typedef SimpleNode _NodeFactory(String path);
+typedef LocalNode NodeFactory(String path);
+typedef SimpleNode SimpleNodeFactory(String path);
 
 /// A simple table result.
 /// This is used to return simple tables from an action.
@@ -92,6 +94,8 @@ abstract class MutableNodeProvider {
   LocalNode addNode(String path, Map m);
   /// Removes the node specified at [path].
   void removeNode(String path);
+  // Add a profile to the node provider.
+  void addProfile(String name, NodeFactory factory);
 }
 
 class SimpleNodeProvider extends NodeProviderImpl
@@ -128,6 +132,11 @@ class SimpleNodeProvider extends NodeProviderImpl
     }
   }
 
+  @override
+  void addProfile(String name, SimpleNodeFactory factory) {
+    _profiles[name] = factory;
+  }
+
   /// Creates a node at [path].
   /// If a node already exists at this path, an exception is thrown.
   SimpleNode createNode(String path) {
@@ -158,7 +167,7 @@ class SimpleNodeProvider extends NodeProviderImpl
   /// Creates a [SimpleNodeProvider].
   /// If [m] and optionally [profiles] is specified,
   /// the provider is initialized with these values.
-  SimpleNodeProvider([Map m, Map profiles]) {
+  SimpleNodeProvider([Map m, Map<String, SimpleNodeFactory> profiles]) {
     // by default, the first SimpleNodeProvider is the static instance
     if (instance == null) {
        instance = this;
@@ -184,9 +193,13 @@ class SimpleNodeProvider extends NodeProviderImpl
   SimpleHiddenNode sys;
 
   @override
-  void init([Map m, Map profiles]) {
+  void init([Map m, Map<String, SimpleNodeFactory> profiles]) {
     if (profiles != null) {
-      _registerProfiles(profiles);
+      if (profiles.isNotEmpty) {
+        _profiles.addAll(profiles);
+      } else {
+        _profiles = profiles;
+      }
     }
 
     if (m != null) {
@@ -234,8 +247,8 @@ class SimpleNodeProvider extends NodeProviderImpl
 
     if (node == null) {
       String profile = m[r'$is'];
-      if (_profileFactories.containsKey(profile)) {
-        node = _profileFactories[profile](path);
+      if (_profiles.containsKey(profile)) {
+        node = _profiles[profile](path);
       } else {
         node = getOrCreateNode(path);
       }
@@ -278,15 +291,7 @@ class SimpleNodeProvider extends NodeProviderImpl
     nodes.remove(path);
   }
 
-  Map<String, _NodeFactory> _profileFactories = new Map<String, _NodeFactory>();
-
-  void _registerProfiles(Map m) {
-    m.forEach((key, val) {
-      if (key is String && val is _NodeFactory) {
-        _profileFactories[key] = val;
-      }
-    });
-  }
+  Map<String, _NodeFactory> _profiles = new Map<String, _NodeFactory>();
 
   /// Permissions
   IPermissionManager permissions = new DummyPermissionManager();
@@ -595,7 +600,7 @@ class SimpleNode extends LocalNodeImpl {
   /// - [SimpleTableResult]
   /// - [AsyncTableResult]
   ///
-  /// You can also return a future (like if the method is async) of the following types:
+  /// You can also return a future that resolves to one (like if the method is async) of the following types:
   /// - [Stream]
   /// - [Iterable]
   /// - [Map]
