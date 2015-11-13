8eece3e43eeca1bc5143c56a8a89cdf572d58fe9
Kenneth Endfinger <kaendfinger@gmail.com>
2015-11-13 02:39:05 -0500

    Implement the Scheduler.safeEvery API to allow simple time-interval based asynchronous actions.

--- lib/utils.dart
+++ lib/utils.dart
@@ -19,6 +19,8 @@ part "src/utils/dslink_json.dart";
 part "src/utils/list.dart";
 part "src/utils/uri_component.dart";
 
+typedef ExecutableFunction();
+
 /// The DSA Version
 const String DSA_VERSION = '1.1.1';
 
@@ -127,6 +129,23 @@ class Interval {
   int get inMilliseconds => duration.inMilliseconds;
 }
 
+abstract class Disposable {
+  void dispose();
+}
+
+class FunctionDisposable extends Disposable {
+  final ExecutableFunction function;
+
+  FunctionDisposable(this.function);
+
+  @override
+  void dispose() {
+    if (function != null) {
+      function();
+    }
+  }
+}
+
 /// Schedule Tasks
 class Scheduler {
   static Timer get currentTimer => Zone.current["dslink.scheduler.timer"];
@@ -150,6 +169,39 @@ class Scheduler {
 
     return new Timer.periodic(duration, (timer) async {
       await runZoned(action, zoneValues: {"dslink.scheduler.timer": timer});
+    });
+  }
+
+  static Disposable safeEvery(interval, action()) {
+    Duration duration;
+
+    if (interval is Duration) {
+      duration = interval;
+    } else if (interval is int) {
+      duration = new Duration(milliseconds: interval);
+    } else if (interval is Interval) {
+      duration = interval.duration;
+    } else {
+      throw new Exception("Invalid Interval: ${interval}");
+    }
+
+    ExecutableFunction schedule;
+    Timer timer;
+    bool disposed = false;
+    schedule = () async {
+      await action();
+      if (!disposed) {
+        new Timer(duration, schedule);
+      }
+    };
+
+    timer = new Timer(duration, schedule);
+
+    return new FunctionDisposable(() {
+      if (timer != null) {
+        timer.cancel();
+      }
+      disposed = true;
     });
   }
 
