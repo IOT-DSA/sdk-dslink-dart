309b73bf0205c2b7bcc933dc93f4c03c25ffadd9
Rick Zhou <rinick@gmail.com>
2015-11-16 12:51:05 -0800

    add documents to the storage interfaces

--- lib/src/responder/manager/storage.dart
+++ lib/src/responder/manager/storage.dart
@@ -1,24 +1,39 @@
 part of dslink.responder;
 
 
+/// general purpose stroage class
 abstract class IStorageManager {
   /// general key/value pair storage
   IValueStorageBucket getOrCreateValueStorageBucket(String name);
   void destroyValueStorageBucket(String name);
   
-  ISubscriptionResponderStorage getOrCreateSubscriptionStorage(String rpath);
-  void destroySubscriptionStorage(String rpath);
+  /// get subscription storage
+  /// responder path point to a local responder node
+  /// which means the dslink on the other side of the connection is a requester
+  ISubscriptionResponderStorage getOrCreateSubscriptionStorage(String responderPath);
+  
+  /// destroy subscription storage
+  void destroySubscriptionStorage(String responderPath);
+  
+  /// load all saved subscriptions
+  /// should be called only during applicaiton initialization
   Future<List<List<ISubscriptionNodeStorage>>> loadSubscriptions();
 }
 
+/// a storage container for one dslink
+/// different dslink will have different ISubscriptionResponderStorage
 abstract class ISubscriptionResponderStorage {
   String get responderPath;
-  ISubscriptionNodeStorage getOrCreateValue(String path);
-  void destroyValue(String path);
+  
+  ISubscriptionNodeStorage getOrCreateValue(String valuePath);
+  void destroyValue(String valuePath);
+  /// load all saved subscriptions
+  /// should be called only during applicaiton initialization
   Future<List<ISubscriptionNodeStorage>> load();
   void destroy();
 }
 
+/// the storage of one value
 abstract class ISubscriptionNodeStorage {
   final String path;
   final ISubscriptionResponderStorage storage;
@@ -28,19 +43,32 @@ abstract class ISubscriptionNodeStorage {
   /// add data to List of values
   void addValue(ValueUpdate value);
   
+  /// set value to newValue and clear all existing values in the storage
+  /// [removes] is only designed for databse that can't directly remove all data in a key.
+  /// ValueUpdate.storedData can be used to store any helper data for the storage class
   void setValue(Iterable<ValueUpdate> removes, ValueUpdate newValue);
   
+  /// for some database it's easier to remove data one by one
+  /// removeValue and valueRemoved will be both called, either one can be used
   void removeValue(ValueUpdate value);
-  /// api to optimize file remove;
+  
+  /// for some database it's easier to remove multiple data together
+/// removeValue and valueRemoved will be both called, either one can be used
   void valueRemoved(Iterable<ValueUpdate> updates){}
   
   
   void clear(int qos);
   void destroy();
-  /// return the local vlaues
+  
+  /// return the existing storage vlaues
+  /// should be called only during applicaiton initialization
+  /// and value will only be availibe after parent's load() function is finished
   List<ValueUpdate> getLoadedValues();
 }
 
+
+/// a storage class for general purpose key/value pair
+/// TODO, should we add class to maintain a single value so the set/remove is more efficient?
 abstract class IValueStorageBucket {
   void setValue(String key, Object value);
   void removeValue(String key);
