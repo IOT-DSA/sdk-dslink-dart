eb54ee445b9fc205d92051ed4e641dc48a05559b
Kenneth Endfinger <kaendfinger@gmail.com>
2015-12-08 16:53:48 -0500

    Add SynchronousDataStorage and use it for Private Keys when possible.

--- lib/browser_client.dart
+++ lib/browser_client.dart
@@ -31,8 +31,23 @@ abstract class DataStorage {
   Future store(String key, String value);
 }
 
+/// A Synchronous Storage System for DSA Data
+abstract class SynchronousDataStorage {
+  /// Get a key's value.
+  String getSync(String key);
+
+  /// Check if a key is stored.
+  bool hasSync(String key);
+
+  /// Remove the specified key.
+  String removeSync(String key);
+
+  /// Store a key value pair.
+  void storeSync(String key, String value);
+}
+
 /// Storage for DSA in Local Storage
-class LocalDataStorage extends DataStorage {
+class LocalDataStorage extends DataStorage implements SynchronousDataStorage {
   static final LocalDataStorage INSTANCE = new LocalDataStorage();
 
   LocalDataStorage();
@@ -43,9 +58,6 @@ class LocalDataStorage extends DataStorage {
   @override
   Future<bool> has(String key) async => window.localStorage.containsKey(key);
 
-  bool hasSync(String key) => window.localStorage.containsKey(key);
-  String getSync(String key) => window.localStorage[key];
-
   @override
   Future store(String key, String value) {
     window.localStorage[key] = value;
@@ -53,7 +65,25 @@ class LocalDataStorage extends DataStorage {
   }
 
   @override
-  Future<String> remove(String key) async => window.localStorage.remove(key);
+  Future<String> remove(String key) async {
+    return window.localStorage.remove(key);
+  }
+
+  @override
+  String removeSync(String key) {
+    return window.localStorage.remove(key);
+  }
+
+  @override
+  void storeSync(String key, String value) {
+    window.localStorage[key] = value;
+  }
+
+  @override
+  bool hasSync(String key) => window.localStorage.containsKey(key);
+
+  @override
+  String getSync(String key) => window.localStorage[key];
 }
 
 PrivateKey _cachedPrivateKey;
@@ -72,13 +102,33 @@ Future<PrivateKey> getPrivateKey({DataStorage storage}) async {
   String keyLockPath = "dsa_key_lock:${window.location.pathname}";
   String randomToken = "${new DateTime.now().millisecondsSinceEpoch} ${DSRandom.instance.nextUint16()} ${DSRandom.instance.nextUint16()}";
 
-  var hasKeyPath = storage is LocalDataStorage ? storage.hasSync(keyPath) : await storage.has(keyPath);
+  bool hasKeyPath = false;
+
+  if (storage is SynchronousDataStorage) {
+    hasKeyPath = (storage as SynchronousDataStorage).hasSync(keyPath);
+  } else {
+    hasKeyPath = await storage.has(keyPath);
+  }
 
   if (hasKeyPath) {
-    await storage.store(keyLockPath, randomToken);
+    if (storage is SynchronousDataStorage) {
+      (storage as SynchronousDataStorage).storeSync(keyLockPath, randomToken);
+    } else {
+      await storage.store(keyLockPath, randomToken);
+    }
+
     await new Future.delayed(const Duration(milliseconds: 20));
-    var existingToken = storage is LocalDataStorage ? storage.getSync(keyLockPath) : await storage.get(keyLockPath);
-    var existingKey = storage is LocalDataStorage ? storage.getSync(keyPath) : await storage.get(keyPath);
+    String existingToken;
+    String existingKey;
+
+    if (storage is SynchronousDataStorage) {
+      existingToken = (storage as SynchronousDataStorage).getSync(keyLockPath);
+      existingKey = (storage as SynchronousDataStorage).getSync(keyPath);
+    } else {
+      existingToken = await storage.get(keyLockPath);
+      existingKey = await storage.get(keyPath);
+    }
+
     if (existingToken == randomToken) {
       if (storage is LocalDataStorage) {
         _startStorageLock(keyLockPath, randomToken);
@@ -94,8 +144,14 @@ Future<PrivateKey> getPrivateKey({DataStorage storage}) async {
   _cachedPrivateKey = await PrivateKey.generate();
 
   if (keyLockPath != null) {
-    storage.store(keyPath, _cachedPrivateKey.saveToString());
-    storage.store(keyLockPath, randomToken);
+    if (storage is SynchronousDataStorage) {
+      (storage as SynchronousDataStorage).storeSync(keyPath, _cachedPrivateKey.saveToString());
+      (storage as SynchronousDataStorage).storeSync(keyLockPath, randomToken);
+    } else {
+      await storage.store(keyPath, _cachedPrivateKey.saveToString());
+      await storage.store(keyLockPath, randomToken);
+    }
+
     if (storage is LocalDataStorage) {
       _startStorageLock(keyLockPath, randomToken);
     }
