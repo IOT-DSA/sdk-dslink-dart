175b92d847c0ebda9933744d44ebf98a8e63eb62
Rick Zhou <rinick@gmail.com>
2018-07-27 09:05:02 -0700

    remove template function to make it compatible with old compiler

--- lib/src/utils/promise_timeout.dart
+++ lib/src/utils/promise_timeout.dart
@@ -1,10 +1,10 @@
 part of dslink.utils;
 
-Future<T> awaitWithTimeout<T>(Future<T> future, int timeoutMs,
-    {Function onTimeout = null,
-    Function onSuccessAfterTimeout = null,
-    Function onErrorAfterTimeout = null}) {
-  Completer<T> completer = new Completer();
+Future awaitWithTimeout(Future future, int timeoutMs,
+    {Function onTimeout: null,
+    Function onSuccessAfterTimeout: null,
+    Function onErrorAfterTimeout: null}) {
+  Completer completer = new Completer();
 
   Timer timer = new Timer(new Duration(milliseconds: timeoutMs), () {
     if (!completer.isCompleted) {
@@ -14,7 +14,7 @@ Future<T> awaitWithTimeout<T>(Future<T> future, int timeoutMs,
       completer.completeError(new Exception('Future timeout before complete'));
     }
   });
-  future.then((T t) {
+  future.then((t) {
     if (completer.isCompleted) {
       if (onSuccessAfterTimeout != null) {
         onSuccessAfterTimeout(t);
