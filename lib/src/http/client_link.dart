a0570b4477f284d975d51160264d87d06f0643f9
Matthew Butler <butler.matthew@gmail.com>
2018-08-22 14:53:03 -0300

    Revert indentation fix

--- lib/src/http/client_link.dart
+++ lib/src/http/client_link.dart
@@ -149,64 +149,64 @@ class HttpClientLink extends ClientLink {
     // https://github.com/dart-lang/sdk/issues/31275
     // When it is fixed, we should go back to a regular try-catch
     runZoned(() async {
-      await ()async{
-        HttpClientRequest request = await client.postUrl(connUri);
-        Map requestJson = {
-          'publicKey': privateKey.publicKey.qBase64,
-          'isRequester': requester != null,
-          'isResponder': responder != null,
-          'formats': formats,
-          'version': DSA_VERSION,
-          'enableWebSocketCompression': true
-        };
+    await ()async{
+      HttpClientRequest request = await client.postUrl(connUri);
+      Map requestJson = {
+        'publicKey': privateKey.publicKey.qBase64,
+        'isRequester': requester != null,
+        'isResponder': responder != null,
+        'formats': formats,
+        'version': DSA_VERSION,
+        'enableWebSocketCompression': true
+      };
 
-        if (linkData != null) {
-          requestJson['linkData'] = linkData;
-        }
+      if (linkData != null) {
+        requestJson['linkData'] = linkData;
+      }
 
-        logger.finest(formatLogMessage("Handshake Request: ${requestJson}"));
-        logger.fine(formatLogMessage("ID: ${dsId}"));
+      logger.finest(formatLogMessage("Handshake Request: ${requestJson}"));
+      logger.fine(formatLogMessage("ID: ${dsId}"));
 
-        request.add(toUTF8(DsJson.encode(requestJson)));
-        HttpClientResponse response = await request.close();
-        List<int> merged = await response.fold(<int>[], foldList);
-        String rslt = const Utf8Decoder().convert(merged);
-        Map serverConfig = DsJson.decode(rslt);
+      request.add(toUTF8(DsJson.encode(requestJson)));
+      HttpClientResponse response = await request.close();
+      List<int> merged = await response.fold(<int>[], foldList);
+      String rslt = const Utf8Decoder().convert(merged);
+      Map serverConfig = DsJson.decode(rslt);
 
-        logger.finest(formatLogMessage("Handshake Response: ${serverConfig}"));
+      logger.finest(formatLogMessage("Handshake Response: ${serverConfig}"));
 
-        saltNameMap.forEach((name, idx) {
-          //read salts
-          salts[idx] = serverConfig[name];
-        });
-        String tempKey = serverConfig['tempKey'];
-        if (tempKey == null) {
-          // trusted client, don't do ECDH handshake
-          _nonce = const DummyECDH();
-        } else {
-          _nonce = await privateKey.getSecret(tempKey);
-        }
-        // server start to support version since 1.0.4
-        // and this is the version ack is added
-        enableAck = serverConfig.containsKey('version');
-        remotePath = serverConfig['path'];
-
-        if (serverConfig['wsUri'] is String) {
-          _wsUpdateUri = '${connUri.resolve(serverConfig['wsUri'])}?dsId=${Uri.encodeComponent(dsId)}'
-              .replaceFirst('http', 'ws');
-          if (home != null) {
-            _wsUpdateUri = '$_wsUpdateUri&home=$home';
-          }
-        }
-
-        if (serverConfig['format'] is String) {
-          format = serverConfig['format'];
-        }
-      }().timeout(new Duration(minutes: 1), onTimeout:(){
-          client.close(force: true);
-          throw new TimeoutException('Connection to $_conn',
-              const Duration(minutes: 1));
+      saltNameMap.forEach((name, idx) {
+        //read salts
+        salts[idx] = serverConfig[name];
       });
+      String tempKey = serverConfig['tempKey'];
+      if (tempKey == null) {
+        // trusted client, don't do ECDH handshake
+        _nonce = const DummyECDH();
+      } else {
+        _nonce = await privateKey.getSecret(tempKey);
+      }
+      // server start to support version since 1.0.4
+      // and this is the version ack is added
+      enableAck = serverConfig.containsKey('version');
+      remotePath = serverConfig['path'];
+
+      if (serverConfig['wsUri'] is String) {
+        _wsUpdateUri = '${connUri.resolve(serverConfig['wsUri'])}?dsId=${Uri.encodeComponent(dsId)}'
+            .replaceFirst('http', 'ws');
+        if (home != null) {
+          _wsUpdateUri = '$_wsUpdateUri&home=$home';
+        }
+      }
+
+      if (serverConfig['format'] is String) {
+        format = serverConfig['format'];
+      }
+    }().timeout(new Duration(minutes: 1), onTimeout:(){
+        client.close(force: true);
+        throw new TimeoutException('Connection to $_conn',
+            const Duration(minutes: 1));
+    });
       await initWebsocket(false);
     }, onError: (e, s) {
       if (logger.level <= Level.FINER ) {
